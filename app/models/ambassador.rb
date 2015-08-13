d0a364336a0c1a2a1b730c950b454e30087286a2
Matt Vasseur <matt@marmar.is>
2015-08-13 19:51:59 -0400

    initials + token

--- app/models/ambassador.rb
+++ app/models/ambassador.rb
@@ -1,8 +1,18 @@
 class Ambassador < ActiveRecord::Base
+  # Callbacks
+  before_create :set_unique_token
+
   # Associations
   has_one :account, as: :meta, dependent: :destroy
-  
+
   belongs_to :ambassador, as: :parent
   has_many :ambassadors, as: :children
+
+  private
+  def create_unique_identifier
+    begin
+      self.token = self.initials + rand(0..999)
+    end while self.class.exists?(token: => token)
+  end
 
 end
