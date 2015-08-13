27056ab2ece1c46bb554c213244c109d70e627c5
Matt Vasseur <matt@marmar.is>
2015-08-13 19:18:42 -0400

    rails g accounts & add fields

---
+++ db/migrate/20150813231730_add_fields_to_account.rb
@@ -0,0 +1,14 @@
+class AddFieldsToAccount < ActiveRecord::Migration
+  def change
+    add_column :accounts, :name, :string, default: ""
+    add_column :accounts, :phone, :string, default: ""
+
+    # Address
+    add_column :accounts, :street, :string, default: ""
+    add_column :accounts, :city, :string, default: ""
+    add_column :accounts, :state, :string, default: ""
+    add_column :accounts, :zip, :string, default: ""
+
+    add_column :accounts, :dob, :date
+  end
+end
