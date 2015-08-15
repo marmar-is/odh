3e1733198cf8c75dc5896e37e7478e6133303f4d
Matt Vasseur <matt@marmar.is>
2015-08-14 23:16:37 -0400

    environmental variables to secrets.yml

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '2525',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => Rails.application.secrets.sendgrid_username,
+  :password       => Rails.application.secrets.sendgrid_password,
   :domain         => 'odh.com',
   :enable_starttls_auto => true
 }
