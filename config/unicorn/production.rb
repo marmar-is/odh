f1c253bcf1c89e615026673b47e6acd7c9f98417
Matt Vasseur <matt@marmar.is>
2015-08-16 15:54:09 -0400

    cap install etc.

---
+++ config/unicorn/production.rb
@@ -0,0 +1,39 @@
+root = "/home/marmaris/apps/odh/current"
+shared_path= "/home/marmaris/apps/odh/shared"
+working_directory root
+
+pid "#{shared_path}/pids/unicorn.pid"
+
+stderr_path "#{shared_path}/logs/unicorn.error.log"
+stdout_path "#{shared_path}/logs/unicorn.access.log"
+
+worker_processes Integer(ENV['WEB_CONCURRENCY'])
+timeout 60
+preload_app true
+
+listen "#{shared_path}/sockets/unicorn.odh.sock", backlog: 64
+
+before_fork do |server, worker|
+  #Signal.trap 'TERM' do
+  #  puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+  #  Process.kill 'QUIT', Process.pid
+  #end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  #Signal.trap 'TERM' do
+  #  puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  #end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+end
