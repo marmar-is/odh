6b742b78cb70f904111a872344ff9135f45d4b47
Matt Vasseur <matt@marmar.is>
2015-08-17 01:01:21 -0400

    production default mail url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # Default mailer url
-  config.action_mailer.default_url_options = { host: '104.154.78.68' }
+  config.action_mailer.default_url_options = { host: 'proto.marmar.is' }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
