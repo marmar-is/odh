78282f54316bb8a66d5b7bb7d39b54701f72b1e2
Matt Vasseur <matt@marmar.is>
2015-08-17 10:53:44 -0400

    production twilio number

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -16,7 +16,7 @@ class DefaultMailer < ApplicationMailer
   # Send the new referree a text message
   def send_new_referral_text(referrer, referree)
     $twilio_client.account.messages.create({
-      from: "+15005550006", # Change to one of our numbers on production "+13105983640",
+      from: "+13105983640", # Change to one of our numbers on production "+15005550006",
       to: "#{referree.phone}",
       body: "Hello, you have been invited to use ODH by #{referrer.full_name}. Sign Up by clicking the following link: #{new_account_registration_url(registration_token: referree.registration_token, id: referree.id)}",
     })
