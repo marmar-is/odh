d448bab850ecbcc60043b4b2720e97a939f47fe1
Matt Vasseur <matt@marmar.is>
2015-08-18 22:21:21 -0400

    add subscription id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816215622) do
+ActiveRecord::Schema.define(version: 20150818041031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,12 +38,16 @@ ActiveRecord::Schema.define(version: 20150816215622) do
     t.datetime "updated_at",                          null: false
     t.integer  "meta_id"
     t.string   "meta_type"
+    t.string   "stripe_account_id"
+    t.string   "stripe_subscription_id"
   end
 
   add_index "accounts", ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true, using: :btree
   add_index "accounts", ["email"], name: "index_accounts_on_email", unique: true, using: :btree
   add_index "accounts", ["meta_id", "meta_type"], name: "index_accounts_on_meta_id_and_meta_type", using: :btree
   add_index "accounts", ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true, using: :btree
+  add_index "accounts", ["stripe_account_id"], name: "index_accounts_on_stripe_account_id", using: :btree
+  add_index "accounts", ["stripe_subscription_id"], name: "index_accounts_on_stripe_subscription_id", using: :btree
   add_index "accounts", ["unlock_token"], name: "index_accounts_on_unlock_token", unique: true, using: :btree
 
   create_table "ambassadors", force: :cascade do |t|
