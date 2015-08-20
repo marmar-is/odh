51fc39ce40c7707f3fdfce55d1859ad6876bb631
Matt Vasseur <matt@marmar.is>
2015-08-20 16:12:21 -0400

    rake db:migrat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818041031) do
+ActiveRecord::Schema.define(version: 20150820201110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,5 +72,21 @@ ActiveRecord::Schema.define(version: 20150818041031) do
   add_index "ambassadors", ["parent_id"], name: "index_ambassadors_on_parent_id", using: :btree
   add_index "ambassadors", ["registration_token"], name: "index_ambassadors_on_registration_token", unique: true, using: :btree
   add_index "ambassadors", ["token"], name: "index_ambassadors_on_token", unique: true, using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
 end
