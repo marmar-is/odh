5892c27cbcdb0df21c8ba9726cdaf245d60ce152
Matt Vasseur <matt@marmar.is>
2015-08-23 16:37:26 -0400

    payouts matrices

--- app/jobs/payout_job.rb
+++ app/jobs/payout_job.rb
@@ -4,59 +4,30 @@ class PayoutJob < ActiveJob::Base
 
   # Given a new referral, 'thank' the parents who referred him ( and their parents, etc. )
   def perform(new_referral)
-    # Payout Matrix
-      # generation: number of referrers ago
-      # amount:     amount to pay (in cents)
-    payout_matrix = [
-      { generation: 1, amount: 3000 },
-      { generation: 2, amount: 2500 },
-      { generation: 3, amount: 2000 },
-      { generation: 4, amount: 1500 },
-      { generation: 5, amount: 1000 }
-    ]
-
     # Marmaris' customer account for payouts
     marmaris_customer = "cus_6pfktmXjY8Hozs"
 
     # Associated Transaction
     #sub_id = Stripe::Customer.retrieve(new_referral.account.stripe_subscription_id).subscriptions.data[0].id
 
-    first_referrer = new_referral.parent
-    if first_referrer
-      transfer(first_referrer.account.stripe_account_id, 1)
-      second_referrer = first_referrer.parent
+    referrer = new_referral.parent
+    PayoutMatrix.select(:generation, :amount).each do |p|
+      if referrer
+        transfer(referrer.stripe_account_id, p.generation, p.amount)
 
-      if second_referrer
-        transfer(second_referrer.account.stripe_account_id, 2)
-        third_referrer = first_referrer.parent
-
-        if third_referrer
-          transfer(third_referrer.account.stripe_account_id, 3)
-          fourth_referrer = first_referrer.parent
-
-          if fourth_referrer
-            transfer(fourth_referrer.account.stripe_account_id, 4)
-            fifth_referrer = first_referrer.parent
-
-            if fifth_referrer
-              transfer(fifth_referrer.account.stripe_account_id, 5)
-            end
-            # fifth
-          end
-          # fourth
-        end
-        # third
+        referrer = referrer.parent
+      else
+        break
       end
-      # second
     end
-    # first
+
   end
   # perform
 
   private
-  def transfer(stripe_account_id, generation)
+  def transfer(stripe_account_id, generation, amount)
     Stripe::Transfer.create(
-      amount:               payout_matrix[generation-1][:amount],
+      amount:               amount,
       currency:             'USD',
       description:          'Compensation for a referral. Thanks!',
       metadata:             {
--- app/models/payout_matrix.rb
+++ app/models/payout_matrix.rb
@@ -1,4 +1,7 @@
 class PayoutMatrix < ActiveRecord::Base
+  # Order
+  default_scope { order('generation ASC') }
+
   # Validations
   validates :generation, presence: true, numericality: { greater_than_or_equal_to: 1 }, uniqueness: true
   validates :amount,     presence: true, numericality: { greater_than_or_equal_to: 0 }
